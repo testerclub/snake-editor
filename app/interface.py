96abfb5bc28cc4721c63696d02fb8e1fbd156277
Marcio Dantas <84141090+marc-dantas@users.noreply.github.com>
2021-10-26 12:25:07 -0300

    top menu and start message

--- app/interface.py
+++ app/interface.py
@@ -1,6 +1,7 @@
 from tkinter import *
+from pygments import *
 from tkinter.messagebox import *
-from info import min_size, gui_title
+from info import min_size, gui_title, __msg__
 
 class Main(Tk):
 
@@ -15,7 +16,32 @@ class Main(Tk):
         self.settings = None
         self.code = None
         self.run = None
+        self.mainmenu = None
+        self.filemenu = None
+        self.editmenu = None
+        self.aboutmenu = None
     
+    def create_menu_bar(self):
+        self.mainmenu = Menu(self)
+        self.filemenu = Menu(self.mainmenu, tearoff=0)
+        self.filemenu.add_command(label="Open")
+        self.filemenu.add_command(label="Save")
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Exit", command=exit)
+        self.mainmenu.add_cascade(label="File", menu=self.filemenu)
+
+        self.editmenu = Menu(self.mainmenu, tearoff=0)
+        self.editmenu.add_command(label="Font...")
+        self.editmenu.add_command(label="")
+        self.mainmenu.add_cascade(label="Edit", menu=self.editmenu)
+        
+        self.aboutmenu = Menu(self.mainmenu, tearoff=0)
+        self.aboutmenu.add_command(label="Credits")
+        self.aboutmenu.add_command(label="About Snake Editor")
+        self.mainmenu.add_cascade(label="About", menu=self.aboutmenu)
+        
+        self.config(menu=self.mainmenu)
+
     def create_top_bar(self):
         self.open = Button(self, text="I", font=("Aquawax Pro Pictograms", 30),
                             bg='#222222', fg='white')
@@ -29,8 +55,8 @@ class Main(Tk):
         self.save.bind("<Leave>", lambda event: self.save.configure(bg='#222222'))
         self.save.place(relx=.1, relwidth=.1, relheight=.1)
 
-        self.run = Button(self, text="H", font=("Aquawax Pro Pictograms", 30),
-                            bg='#222222', fg='white')
+        self.run = Button(self, text="X", font=("Aquawax Pro Pictograms", 30),
+                            bg='#222222', fg='white', command=exit)
         self.run.bind("<Enter>", lambda event: self.run.configure(bg='gray'))
         self.run.bind("<Leave>", lambda event: self.run.configure(bg='#222222'))
         self.run.place(relx=.8, relwidth=.1, relheight=.1)
@@ -42,11 +68,14 @@ class Main(Tk):
         self.settings.place(relx=.9, relwidth=.1, relheight=.1)
 
     def create_text_editor(self):
-        self.code = Text(self, bg="#111111", fg='white', insertbackground='white')
+        self.code = Text(self, bg="#111111", fg='white', insertbackground='white',
+                            undo=True)
+        self.code.insert(INSERT, __msg__)
         self.code.place(rely=.1, relwidth=1, relheight=1)
 
 
 a = Main(min_size, gui_title)
+a.create_menu_bar()
 a.create_top_bar()
 a.create_text_editor()
 a.mainloop()
