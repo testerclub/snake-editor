00a9b961f2f3edb483d2542df885bbbb04efbc5e
Marcio Dantas <84141090+marc-dantas@users.noreply.github.com>
2021-10-28 19:47:32 -0300

    auto indent fix (for linux)

--- app/engine.py
+++ app/engine.py
@@ -42,7 +42,7 @@ def auto_indent(event):
     match = re.match(r'^(\s+)', line)
     current_indent = len(match.group(0)) if match else 0
     new_indent = current_indent + 4
-    widget.insert("insert", event.char + "\n" + " "*new_indent)
+    widget.insert("insert", "\n" + " "*new_indent)
     return "break"
 
 
